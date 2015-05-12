7800168c7880a51723b7b23877794762acf29485
oleg_fisher <rv_oleg@ukr.net>
2015-05-12 04:01:55 +0300

    Додано редагування, пагінацію, виправлено додавання

--- www/application/views/admin/admin_footer.php
+++ www/application/views/admin/admin_footer.php
@@ -38,6 +38,25 @@
     </div>
 </div>
 
+<div class="modal fade" id="confirm-edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4><?=$this->lang->line('list_edit')?></h4>
+            </div>
+            <div class="modal-body">
+                <div id="editorFields">
+                  
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><?=$this->lang->line('list_remove_cancel')?></button>
+                <a class="btn btn-primary primary confirm-edit"><?=$this->lang->line('list_edit_confirm')?></a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="/static/jquery/jquery.min.js"></script>
 <script src="/static/admin/script/bootstrap.min.js"></script>
 <script src="/static/admin/script/dashboard.js"></script>
